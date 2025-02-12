b3175cc532310d97e9c79f7e3c2d0b79e1ff369c
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2025-02-11 19:36:24 -0700

    clean files

--- src/optimization/example_arg.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(prog='SOS Optimizer')
-parser.add_argument('date')
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    print(f"The passed date is {args.date}")
-
--- src/optimization/example_env.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-date = os.environ.get("date", "default")
-print(f"The environ var date is {date}")
